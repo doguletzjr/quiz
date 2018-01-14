78130ac8d9922d488195edd4276185cae65bbcab
doguletzjr <30929017+doguletzjr@users.noreply.github.com>
2018-01-14 21:37:57 +0200

    score

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 $(document).ready(function(){
 $('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-$("#results").click(function() {
-
-if (!$("input[@name=q1]:checked").val() ||
-!$("input[@name=q2]:checked").val() ||
-!$("input[@name=q3]:checked").val() ||
-!$("input[@name=q4]:checked").val() ||
-!$("input[@name=q5]:checked").val() ||
-!$("input[@name=q6]:checked").val() ||
-!$("input[@name=q7]:checked").val() ||
-!$("input[@name=q8]:checked").val() ||
-!$("input[@name=q9]:checked").val()
-) {
-alert("You're not done yet!");
-}
-var cat1 = ($("input[@name=q1]:checked").val() != "3");
-
-var cat2 = ($("input[@name=q2]:checked").val() != "8");
-
-var cat3 = ($("input[@name=q3]:checked").val() != "4");
-
-var cat4 = ($("input[@name=q4]:checked").val() != "8");
-
-var cat5 = ($("input[@name=q5]:checked").val() != "4");
-
-var cat6 = ($("input[@name=q6]:checked").val() != "8");
-
-var cat7 = ($("input[@name=q7]:checked").val() != "2");
-
-var cat8 = ($("input[@name=q8]:checked").val() != "7");
-
-var cat9 = ($("input[@name=q9]:checked").val() != "8");
-
-var cat10 = (!cat1 && !cat2 && !cat3 && !cat4 && !cat5 && !cat6 && !cat7 && !cat8 && !cat9);
-alert cat10;
-});});
+  });
+  $("#results").click(function() {
+  if (!$("input[@name=q1]:checked").val() ||
+  !$("input[@name=q2]:checked").val() ||
+  !$("input[@name=q3]:checked").val() ||
+  !$("input[@name=q4]:checked").val() ||
+  !$("input[@name=q5]:checked").val() ||
+  !$("input[@name=q6]:checked").val() ||
+  !$("input[@name=q7]:checked").val() ||
+  !$("input[@name=q8]:checked").val() ||
+  !$("input[@name=q9]:checked").val()
+  ) {
+  alert("You're not done yet!");
+  } else {
+  var answears = 0;
+  if ($("input[@name=q1]:checked").val() == "3"){
+    answears++
+  };
+  if ($("input[@name=q2]:checked").val() == "8"){
+    answears++
+  };
+  if ($("input[@name=q3]:checked").val() == "4"){
+    answears++
+  };
+  if ($("input[@name=q4]:checked").val() == "8"){
+    answears++
+  };
+  if ($("input[@name=q5]:checked").val() == "4"){
+    answears++
+  };
+  if ($("input[@name=q6]:checked").val() == "8"){
+    answears++
+  };
+  if ($("input[@name=q7]:checked").val() == "2"){
+    answears++
+  };
+  if ($("input[@name=q8]:checked").val() == "7"){
+    answears++
+  };
+  if ($("input[@name=q9]:checked").val() == "8"){
+    answears++
+  };
+  var score = 'You answered ' + answears + '/9 correctly!';
+$("#categorylist").text(score);
+$("#categorylist").show("slow");
+  }
+      });});
