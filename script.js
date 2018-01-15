228430a03e5dfc0f0a8336c5df182fc7e67e4281
doguletzjr <30929017+doguletzjr@users.noreply.github.com>
2018-01-15 22:00:03 +0200

    js works!

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ $(document).ready(function(){
 $('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
- $("results").click(function() {
-  if (!$("input[@name=q1]:checked").val() ||
-  !$("input[@name=q2]:checked").val() ||
-  !$("input[@name=q3]:checked").val() ||
-  !$("input[@name=q4]:checked").val() ||
-  !$("input[@name=q5]:checked").val() ||
-  !$("input[@name=q6]:checked").val() ||
-  !$("input[@name=q7]:checked").val() ||
-  !$("input[@name=q8]:checked").val() ||
-  !$("input[@name=q9]:checked").val()
+ $(".results").click(function() {
+  if (!$("input[name='q1']:checked").val() ||
+  !$("input[name='q2']:checked").val() ||
+  !$("input[name='q3']:checked").val() ||
+  !$("input[name='q4']:checked").val() ||
+  !$("input[name='q5']:checked").val() ||
+  !$("input[name='q6']:checked").val() ||
+  !$("input[name='q7']:checked").val() ||
+  !$("input[name='q8']:checked").val() ||
+  !$("input[name='q9']:checked").val()
   ) {
   alert("You're not done yet!");
   } else {
   var answers = 0;
-  if ($("input[@name=q1]:checked").val() == "3"){
+  if ($("input[name='q1']:checked").val() == "3"){
     answers++
   };
-  if ($("input[@name=q2]:checked").val() == "8"){
+  if ($("input[name='q2']:checked").val() == "8"){
     answers++
   };
-  if ($("input[@name=q3]:checked").val() == "4"){
+  if ($("input[name='q3']:checked").val() == "4"){
     answers++
   };
-  if ($("input[@name=q4]:checked").val() == "8"){
+  if ($("input[name='q4']:checked").val() == "8"){
     answers++
   };
-  if ($("input[@name=q5]:checked").val() == "4"){
+  if ($("input[name='q5']:checked").val() == "4"){
     answers++
   };
-  if ($("input[@name=q6]:checked").val() == "8"){
+  if ($("input[name='q6']:checked").val() == "8"){
     answers++
   };
-  if ($("input[@name=q7]:checked").val() == "2"){
+  if ($("input[name='q7']:checked").val() == "2"){
     answers++
   };
-  if ($("input[@name=q8]:checked").val() == "7"){
+  if ($("input[name='q8']:checked").val() == "7"){
     answers++
   };
-  if ($("input[@name=q9]:checked").val() == "8"){
+  if ($("input[name='q9']:checked").val() == "8"){
     answers++
   };
-  var score = 'You answered ' + answears + '/9 correctly!';
-$("#result").text(score);
-$("#result").show("slow");
+  var score = 'You got ' + answers + '/9 questions correctly!';
+$(".results").text(score);
+$(".results").show("slow");
   }
       });});
