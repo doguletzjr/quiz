38de0a800f5259d4bf28cc9f8e6e929fff9b24be
doguletzjr <30929017+doguletzjr@users.noreply.github.com>
2018-01-15 19:59:25 +0200

    b

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 $('.message').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
  });
- $("#results").click(function() {
+ $("results").click(function() {
   if (!$("input[@name=q1]:checked").val() ||
   !$("input[@name=q2]:checked").val() ||
   !$("input[@name=q3]:checked").val() ||
